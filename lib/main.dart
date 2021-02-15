eb2dab39f18e2c2501825efe2edd50ffea1a8d85
kleitonpsld <kleitonpsld@hotmail.com>
2021-02-15 07:55:19 -0300

    remover comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,14 @@ import 'package:flutter/material.dart';
 
 void main() async {
   runApp(MyApp());
-  // Firestore.instance.collection("col").document("doc2").setData({"texto": "como vc esta?"});
-  // Firestore.instance.collection("mensagens").document("msg2").setData({
-  //   'texto': 'blza',
-  //   'from': 'Kleiton',
-  //   'read': false
-  // });
-  // QuerySnapshot snapshot = await Firestore.instance.collection("col").getDocuments();
-  // snapshot.documents.forEach((d) {
-  //   print(d.data);
-  //  });
-  // Firestore.instance.collection('mensagens').snapshots().listen((dado) {
-  //   dado.documents.forEach((d) {
-  //     print(d.data);
-  //    });
-  //  });
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         iconTheme: IconThemeData(
           color: Colors.blue,
